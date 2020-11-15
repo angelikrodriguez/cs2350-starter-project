9754c1442da6ed249d70ebb459e9632a4165228b
angelikrodriguez <54684517+angelikrodriguez@users.noreply.github.com>
2020-11-15 13:17:51 -0700

    Activity8

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,7 +4,32 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
+//TODO
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+// JavaScript
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {movies} from './movies'
 
-//TODO
+
+let featured_movie = document.querySelector('.featured')
+for(let m of movies){
+    let movie_thumb = document.getElementById('m' + m.id)
+    movie_thumb.innerHTML = `
+    <img src="${m.poster}">
+    `
+    movie_thumb.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+
+function selectMovie(m){
+    featured_movie.innerHTML= `
+    <img src="${m.poster}" >
+    <h3>${m.title}</h3>
+    
+    `
+}
+
---
+++ src/javascripts/movies.js
@@ -0,0 +1,74 @@
+export const movies = [{
+    "id": 1,
+    "title": "Orange Coctel",
+    "poster": "https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "id": 2,
+    "title": "Fruits Coctel",
+    "poster": "https://images.unsplash.com/photo-1481671703460-040cb8a2d909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+ 
+   {
+    "id": 3,
+    "title": "Orange Juice",
+    "poster": "https://images.unsplash.com/photo-1600271886742-f049cd451bba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  
+  },
+  {
+    "id": 4,
+    "title": "Chocolate",
+    "poster": "https://images.unsplash.com/photo-1481391032119-d89fee407e44?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    
+  },
+  {
+    "id": 5,
+    "title": "Coffee",
+    "poster": "https://images.unsplash.com/photo-1567310017355-abb57d268c73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+  },
+  {
+    "id": 6,
+    "title": "Red Wine",
+    "poster": "https://images.unsplash.com/photo-1555919807-c3b672ff8933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    
+  },
+  {
+    "id": 7,
+    "title": "Tropical",
+    "poster": "https://images.unsplash.com/photo-1514995428455-447d4443fa7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+ 
+  },
+  {
+    "id": 8,
+    "title": "Grapes - Wine",
+    "poster": "https://images.unsplash.com/photo-1568907047661-1492520913f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  
+  },
+  {
+   "id": 9,
+    "title": "Chocolate",
+    "poster": "https://images.unsplash.com/photo-1481391032119-d89fee407e44?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    
+  },
+  {
+    "id": 10,
+    "title": "Blueberry Conctail",
+    "poster": "https://images.unsplash.com/photo-1513816861509-09733a9f9383?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+  },
+   {
+    "id": 11,
+    "title": "The Godfather: Part II",
+    "poster": "https://images.unsplash.com/photo-1569564358211-b7dee0e568d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  
+  },
+   {
+    "id": 12,
+    "title": "Orange Juice",
+    "poster": "https://images.unsplash.com/photo-1600271886742-f049cd451bba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+  
+  }
+]
+
+
